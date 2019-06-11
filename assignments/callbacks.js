ae720af4c97f80950153eee3eb0b9b246021a4ea
Alexis Panyathong <alexispanyathong@gmail.com>
2019-06-11 17:39:00 -0500

    I have completed all of the callbacks.js EXCEPT for the last one. I am working on the array.method.js and stuck on challenge 2.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,8 +2,6 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
-
   //Given this problem: 
   
   function firstItem(arr, cb) {
@@ -22,29 +20,62 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     console.log(first)
   });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
+function getLength(arr, cb) {
+  return cb(arr.length);
+}
+
+getLength(items, function(length) {
+  console.log(length)
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+function last(arr, cb) {
+  return cb(arr[3]);
+}
+
+last(items, function(last) {
+  console.log(last)
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
+function sumNums(x, y, cb) {
+  return cb(x, y);
+}
+const add = function(num1, num2) {
+  return num1 + num2;
+}
+console.log(sumNums(2,4,add));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+function multiplyNums(x, y, cb) {
+  return cb(x, y);
+}
+const multiply = function(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiplyNums(2,4,multiply));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+function contains(item, list, cb) {
+  return cb(item, list)
+}
+
 
 /* STRETCH PROBLEM */
 
